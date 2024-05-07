36d3bdba92b85bf7ed6f41c0e0f4e907f74511a7
nitinpatil2000 <126668836+nitinpatil2000@users.noreply.github.com>
2024-05-07 12:29:00 +0530

    Replaced your api key

--- app/src/main/java/com/courses/trackerappnp/other/Constant.kt
+++ app/src/main/java/com/courses/trackerappnp/other/Constant.kt
@@ -1,5 +1,7 @@
 package com.courses.trackerappnp.other
 
+import android.graphics.Color
+
 object Constant {
 
     const val RUNNING_DATABASE_NAME = "RoomDb"
@@ -21,4 +23,8 @@ object Constant {
     //for location
     const val LOCATION_UPDATE_INTERVAL = 5000L
     const val FASTEST_LOCATION_INTERVAL = 2000L
+
+    const val POLYLINE_COLOR = Color.RED
+    const val POLYLINE_WIDTH = 8f
+    const val MAP_ZOOM = 15f
 }
--- app/src/main/java/com/courses/trackerappnp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/courses/trackerappnp/ui/fragments/TrackingFragment.kt
@@ -1,7 +1,6 @@
 package com.courses.trackerappnp.ui.fragments
 
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,18 +8,25 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.courses.trackerappnp.R
 import com.courses.trackerappnp.databinding.FragmentTrackingBinding
+import com.courses.trackerappnp.other.Constant.ACTION_PAUSE_SERVICE
 import com.courses.trackerappnp.other.Constant.ACTION_START_OR_RESUME_SERVICE
+import com.courses.trackerappnp.other.Constant.MAP_ZOOM
+import com.courses.trackerappnp.other.Constant.POLYLINE_COLOR
+import com.courses.trackerappnp.other.Constant.POLYLINE_WIDTH
+import com.courses.trackerappnp.service.Polyline
 import com.courses.trackerappnp.service.TrackingService
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.PolylineOptions
 
 class TrackingFragment : Fragment(R.layout.fragment_tracking) {
-    private var _binding:FragmentTrackingBinding? = null
+    private var _binding: FragmentTrackingBinding? = null
     private val binding get() = _binding!!
-    private var map: GoogleMap?= null
+    private var map: GoogleMap? = null
+
+    //todo for draw the polyline in the map
+    private var isTracking = false
+    private var pathPoints = mutableListOf<Polyline>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -44,8 +50,72 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
         }
     }
 
+
+    //todo this fun is only connect the two last polylines of our polyline list
+    private fun addLatestPolyline() {
+        if (pathPoints.isNotEmpty() && pathPoints.last().size > 1) {   //todo we need to draw a line at least 2 points
+            val preLastLatLng = pathPoints.last()[pathPoints.last().size - 2]
+            val lastLatLng = pathPoints.last().last()
+            val polyLineOptions = PolylineOptions()
+                .color(POLYLINE_COLOR)
+                .width(POLYLINE_WIDTH)
+                .add(preLastLatLng)
+                .add(lastLatLng)
+
+            map?.addPolyline(polyLineOptions)
+        }
+    }
+
+    //todo this function draws all of our polylines on the map again when we rotate the device
+    private fun addAllPolyline() {    //todo all polyline in the polylines.
+        for (polyline in pathPoints) {
+            val polylineOptions = PolylineOptions()
+                .color(POLYLINE_COLOR)
+                .width(POLYLINE_WIDTH)
+                .addAll(polyline)
+
+            map?.addPolyline(polylineOptions)
+        }
+    }
+
+    //todo to show the line and zoom the camera immediately
+    private fun moveCameraToUser() {
+        if (pathPoints.isNotEmpty() && pathPoints.last().isNotEmpty()) {
+            map?.animateCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                    pathPoints.last().last(),
+                    MAP_ZOOM
+                )
+            )
+        }
+    }
+
+    private fun updateTracking(isTracking:Boolean) {
+        this.isTracking = isTracking
+
+        if (!isTracking) {
+            binding.apply {
+                btnToggleRun.text = "Start"
+                btnFinishRun.visibility = View.VISIBLE
+            }
+        } else {
+            binding.apply {
+                btnToggleRun.text = "Stop"
+                btnFinishRun.visibility = View.GONE
+            }
+        }
+    }
+
+    private fun toggleRun() {
+        if (isTracking) {  //if the service is running then user can be paused this service
+            passedActionToService(ACTION_PAUSE_SERVICE)
+        } else {    //but if the service is paused then user can start or resume the service. 
+
+        }
+    }
+
     //todo use tracking service method pass the action.
-    private fun passedActionToService(action:String){
+    private fun passedActionToService(action: String) {
         Intent(requireContext(), TrackingService::class.java).also {
             it.action = action
             requireContext().startService(it)
